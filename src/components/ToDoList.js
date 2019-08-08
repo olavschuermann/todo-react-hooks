252f603d85447b2cb898eff178ad113340a91060
Olav Schuermann <developer@netzpluslogik.de>
2019-08-08 20:11:20 +0200

    adding todolist and todolistelement components

---
+++ src/components/ToDoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ToDoListElement from './ToDoListElement';
+
+const ToDoListe = () => {
+    return (
+        <>
+            <h1>To-Do-Liste</h1>
+            <ToDoListElement />
+        </>
+    )
+}
+
+
+export default ToDoListe;
---
+++ src/components/ToDoListElement.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const ToDoListElement = () => {
+    return (
+        <h1>ListElement</h1>
+    )
+}
+
+export default ToDoListElement;
