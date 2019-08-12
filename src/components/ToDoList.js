bb29570b80f08ea6718e303deddc3c857c697965
Olav Schuermann <developer@netzpluslogik.de>
2019-08-12 20:48:45 +0200

    adding test data for todo list

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,14 +2,32 @@ import React from 'react';
 
 import ToDoListElement from './ToDoListElement';
 
+const elementList = [
+    {
+        id: 1,
+        task: 'Zeitung holen',
+        done: false
+    },
+    {
+        id: 2,
+        task: 'Fenster putzen',
+        done: false
+    }
+]
+
 const ToDoListe = () => {
     return (
         <>
             <h1>To-Do-Liste</h1>
+            {
+                // .map für jedes Element
+                elementList.map = (elements => {
+                    // TO-DO
+                })
+            }
             <ToDoListElement />
         </>
     )
 }
-
 
 export default ToDoListe;
