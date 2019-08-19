5b282a583e0e2267374f665c3b6dcdbb8b6475b3
Olav Schuermann <developer@netzpluslogik.de>
2019-08-19 16:52:01 +0200

    adding toggle for done or not done

--- src/components/ToDoListElement.js
+++ src/components/ToDoListElement.js
@@ -3,15 +3,21 @@ import React, { useState } from 'react';
 import './ToDo.styles.css';
 
 const ToDoListElement = ({ element }) => {
-  
+
     const [toggleClass, setToggleClass] = useState('todo-notdone');
 
-    const doneToDo = () => {
-       console.log("Clicked!");
-       
-       // props.elementList.done = true;
-       // setToggleClass('todo-done');
-       // Problem: alle todos werden durchgestrichen
+    const toDoToggle = () => {
+        console.log("Clicked!");
+        
+        // Toggle: if true than false and vice versa
+        // Improvement: One liner for toggle
+        if (toggleClass === 'todo-notdone') {
+            setToggleClass('todo-done');
+        } else {
+            setToggleClass('todo-notdone');
+        }
+
+    
     }
     
     return (
@@ -19,7 +25,7 @@ const ToDoListElement = ({ element }) => {
             {
                 <div>
                     <div className={toggleClass}>{element.task}</div>
-                    <button onClick={doneToDo}>Erledigt</button>
+                    <button onClick={toDoToggle}>Erledigt</button>
                 </div>
             }
         </div>
