5c986db56948be1d2165799b10fc8e85355eccbf
Olav Schuermann <developer@netzpluslogik.de>
2019-08-19 19:34:33 +0200

    adding toggle for done or not done

--- src/components/ToDoListElement.js
+++ src/components/ToDoListElement.js
@@ -15,9 +15,7 @@ const ToDoListElement = ({ element }) => {
             setToggleClass('todo-done');
         } else {
             setToggleClass('todo-notdone');
-        }
-
-    
+        }    
     }
     
     return (
